02220c50e9953cbe0ae2a3165651d96beaa46839
felixrajav <felixvictorraj@gmail.com>
2023-10-11 22:21:54 +0530

    FIRST COMMIT

---
+++ src/pages/ResetPassword.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate, useParams } from 'react-router-dom'; 
+import axios from 'axios';
+
+export default function ResetPassword() {
+
+    const [passwordMode, setPasswordMode] = useState(false);
+    const { token } = useParams();
+    const [passwordCheck, setPasswordCheck] = useState([]);
+
+    
+    const Navigate = useNavigate();
+    const handleResetPassword = (event) => {
+        event.preventDefault();
+        if (passwordCheck.newPassword === passwordCheck.confirmPassword) {
+            const RESET_URL = process.env.NODE_ENV === 'development' ? `${process.env.REACT_APP_DEV_URL_FOR_BACKEND}/reset/${token}` : `${process.env.REACT_APP_PRO_URL_FOR_BACKEND}/reset/${token}`;
+            axios.patch(RESET_URL, {password:passwordCheck.newPassword})
+                .then(response => {
+                    if (response.data.success) {
+                        Navigate('/passwordChanged');
+                    }
+                })
+                .catch(err => {
+                    alert("Something Went Wrong",err);
+                });
+        }
+        else {
+            alert("password doesn't match pls check");
+        }
+    }
+    
+
+    return (
+        <div>
+            <div className='resetPassword-box'>
+            
+                <div className='input-box p-5'>
+                    <Form className='mt-1' onSubmit={handleResetPassword}>
+                        <div className="pwd-container">
+                            <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
+                                <Form.Control size='lg' type={passwordMode ? "text" : "password"} placeholder="New Password" onChange={e => setPasswordCheck({ ...passwordCheck, newPassword: e.target.value })} required />
+                            </Form.Group>
+                            
+                        </div>
+                        <div className="pwd-container">
+                            <Form.Group className="mb-5" controlId="exampleForm.ControlInput2">
+                                <Form.Control size='lg' type={passwordMode ? "text" : "password"} placeholder='Confirm Password' onChange={e => setPasswordCheck({ ...passwordCheck, confirmPassword: e.target.value })} required />
+                            </Form.Group>
+                            <img
+                                title={passwordMode ? "Hide password" : "Show password"}
+                                
+                                onClick={() => setPasswordMode(prevState => !prevState)}
+                                alt='password gif'
+                            />
+                        </div>
+                        <div className="d-grid mt-3 text-center">
+                            <Button variant="primary" size="lg" type='submit'>
+                                Reset Password
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    )
+}
